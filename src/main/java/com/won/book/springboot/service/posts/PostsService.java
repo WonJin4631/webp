e8649accf6394112d5955d296b361173440da717
PARK WONJIN <smartwj@naver.com>
2020-07-22 16:03:50 +0900

    0722 web

--- src/main/java/com/won/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/won/book/springboot/service/posts/PostsService.java
@@ -32,6 +32,12 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete (Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해달 게시글이 없습니다. id="+id));
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
@@ -42,4 +48,6 @@ public class PostsService {
     public List<PostsListResponseDto> findAllDesc(){
         return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
+
+
 }
